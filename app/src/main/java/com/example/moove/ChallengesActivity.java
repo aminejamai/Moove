a01d25c2b9148b5b799e3a29a42def34f0eb9098
Amine JAMAI <mr.maj.2015@gmail.com>
2021-05-31 14:01:04 +0100

    challnges  read from firebase

--- app/src/main/java/com/example/moove/ChallengesActivity.java
+++ app/src/main/java/com/example/moove/ChallengesActivity.java
@@ -1,6 +1,7 @@
 package com.example.moove;
 
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
@@ -10,15 +11,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class ChallengesActivity extends AppCompatActivity {
 
-    int numOfGuests=1;
+    int numOfGuests=1, nHour, nMin;
     Dialog addChallenge;
     Dialog guestsNumber;
 
@@ -36,18 +39,37 @@ public class ChallengesActivity extends AppCompatActivity {
         listOfTests.add(new Test("soufiane"));
         listOfTests.add(new Test("smail"));
 
-        listOfTests.add(new Test("amine"));
-        listOfTests.add(new Test("hamza"));
-        listOfTests.add(new Test("soufiane"));
-        listOfTests.add(new Test("smail"));
+        /*
+        *   // code to read data from firebase
+        *   FirebaseFirestore db = FirebaseFirestore.getInstance();
+        *
+        *   db.collection("Challenges").get().
+        *                  .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        *                       @Override
+                                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-        listOfTests.add(new Test("amine"));
-        listOfTests.add(new Test("hamza"));
-        listOfTests.add(new Test("soufiane"));
-        listOfTests.add(new Test("smail"));
+                                        //progressBar.setVisibility(View.GONE);
+
+                                    if(!queryDocumentSnapshots.isEmpty()){
+
+                                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+
+                                    for(DocumentSnapshot d : list){
+
+                                        Test p = d.toObject(Test.class);
+                                        testList.add(p);
+
+                                    }
+
+                                    //adapter.notifyDataSetChanged();
+        *                   }
+        *
+        * */
 
         ListView lsView = findViewById(R.id.listView1);
         lsView.setAdapter(new TestAdapter(this, listOfTests));
+
+
 
     }
 
@@ -89,5 +111,25 @@ public class ChallengesActivity extends AppCompatActivity {
 
         addChallenge.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         addChallenge.show();
+
+    }
+
+    public void showTimePicker(View view){
+        ImageView time = findViewById(R.id.imageView16);
+
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                nHour = c.get(Calendar.HOUR_OF_DAY);
+                nMin = c.get(Calendar.MINUTE);
+
+                TimePickerDialog timePicker = new TimePickerDialog(ChallengesActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    }
+                }, nHour, nMin, false);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/moove/Test.java
+++ app/src/main/java/com/example/moove/Test.java
@@ -3,9 +3,13 @@ package com.example.moove;
 public class Test {
 
     private String name;
+    private String desc;
+    private String localisation;
 
-    public Test(String name){
+    public Test(String name, String desc, String localisation){
+        this.desc = desc;
         this.name = name;
+        this.localisation = localisation;
     }
 
     public String getName() {
@@ -14,5 +18,21 @@ public class Test {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
     }
 }
